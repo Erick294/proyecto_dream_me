9b5da1b49e1e016904dde0963369a158a102a9e3
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-01-09 19:39:47 -0500

    Proyecto

---
+++ app/src/main/java/com/zero/dream_me/Bienvenida.kt
@@ -0,0 +1,11 @@
+package com.zero.dream_me
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Bienvenida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bienvenida)
+    }
+}
