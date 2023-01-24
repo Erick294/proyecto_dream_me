7c4e32657caab37db85c64b0775f22805e452300
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-01-23 19:53:55 -0500

    Arreglos

--- app/src/main/java/com/zero/dream_me/Bienvenida.kt
+++ app/src/main/java/com/zero/dream_me/Bienvenida.kt
@@ -1,11 +1,23 @@
 package com.zero.dream_me
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+import com.zero.dream_me.databinding.ActivityBienvenidaBinding
 
 class Bienvenida : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBienvenidaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bienvenida)
+
+        val video = binding.videoLogo as VideoView
+        val path = "android.resource://" + packageName + "/" + R.raw.logo_video
+        video.setVideoURI(Uri.parse(path))
+
+        video.start()
     }
 }
